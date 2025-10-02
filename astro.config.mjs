a2ff15618afebb113ce310458cc46608ab75fed1
Brody Neumann <brodyneumann@gmail.com>
2025-10-01 21:00:25 -0400

    Add bconf TextMate grammar to shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import bconfTmLang from "./public/bconf.tmLanguage.json";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	markdown: {
+		shikiConfig: {
+			theme: "monokai",
+			langs: [bconfTmLang],
+		},
+	},
+});
