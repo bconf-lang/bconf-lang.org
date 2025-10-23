27a5466128b9dd769085b392dd8b84e831b8799d
Brody Neumann <brodyneumann@gmail.com>
2025-10-22 23:47:15 -0400

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        ignores: ["dist/**", "**/*.d.ts"],
+    },
+
+    // Javascript
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser },
+        rules: {
+            "no-unused-vars": "error",
+        },
+    },
+
+    // Typescript
+    tseslint.configs.recommended,
+
+    // Prettier
+    {
+        plugins: {
+            prettier: prettier,
+        },
+        rules: {
+            "prettier/prettier": "off", // prettier should format on save
+        },
+    },
+
+    // Astro
+    astro.configs.recommended,
+    {
+        files: ["**/*.astro"],
+        languageOptions: {
+            parser: astro.parser,
+            parserOptions: {
+                parser: tseslint.parser,
+                extraFileExtensions: [".astro"],
+                sourceType: "module",
+                ecmaVersion: "latest",
+                project: "./tsconfig.json",
+            },
+        },
+        rules: {
+            "no-undef": "off", // Disable "not defined" errors for specific Astro types that are globally available (ImageMetadata)
+        },
+    },
+]);
