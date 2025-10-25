9dd6c52212d67b6863d49684f9a531a1fcfce83a
Brody Neumann <brodyneumann@gmail.com>
2025-10-25 02:00:30 -0400

    Update theme switcher to have a collapsed dropdown version

--- src/components/ui/icons/index.ts
+++ src/components/ui/icons/index.ts
@@ -1,7 +1,7 @@
+export { default as BrightnessIcon } from "./brightness.astro";
 export { default as ChevronDownIcon } from "./chevron-down.astro";
 export { default as ChevronRightIcon } from "./chevron-right.astro";
 export { default as GithubIcon } from "./github.astro";
-export { default as MonitorIcon } from "./monitor.astro";
 export { default as MoonIcon } from "./moon.astro";
 export { default as SunlightIcon } from "./sunlight.astro";
 
